2c0490bfe534d917f65e453bd2390cc7b953a9d3
Zhouyue_Lyu <zhuoyue.lyu@gmail.com>
2022-12-10 11:48:26 -0500

    all three modes implemented, but need to be tested with real board & system

--- zhuoyue-motor/zhuoyue-motor.ino
+++ zhuoyue-motor/zhuoyue-motor.ino
@@ -11,18 +11,24 @@
 
 // (Step, Pin1, Pin2, Pin3, Pin4)
 // 4 for full step, 8 for half step, explanation here: https://www.motioncontrolonline.org/content-detail.cfm/Motion-Control-Application-Case-Studies/What-is-the-difference-between-full-stepping-the-half-stepping-and-the-micro-drive/content_id/3192
-AccelStepper stepper1(4, 9, 10, 11, 12);
+AccelStepper stepper1(4, 9, 10, 11, 12); // this is the minute (faster) hand
+AccelStepper stepper2(4, 5, 4, 3, 2);    // this is the hour (slower) hand
 
-int speed_1 = 10;
+int speed_1 = 0;
 int speed_2 = 100;
 int speed_3 = 400;
 int curr_speed = 400;
 int max_speed = 400;
+int total_steps = 2048; // this is the number of steps of a full rotation
+bool telepresence_mode = false;
+int time_difference = 5; // time difference in hours
 
 void setup()
 {
    stepper1.setMaxSpeed(max_speed);
    stepper1.setSpeed(curr_speed);
+   stepper2.setMaxSpeed(max_speed);
+   stepper2.setSpeed(curr_speed / 12);
    Serial.begin(9600);
 }
 
@@ -39,10 +45,28 @@ void loop()
       switch (num_faces)
       {
       case 0:
-         update_motor_speed(speed_3);
+         if (telepresence_mode)
+         {
+            update_time_difference(false);
+         }
+         else
+         {
+            update_motor_speed(speed_3);
+         }
          break;
       case 1:
-         update_motor_speed(speed_2);
+         if (telepresence_mode)
+         {
+            update_time_difference(true);
+         }
+         else
+         {
+            update_motor_speed(speed_2);
+         }
+
+         break;
+      case 2:
+         update_motor_speed(speed_1);
          break;
       }
    }
@@ -50,6 +74,7 @@ void loop()
    // of python sending value is probably smaller than the frequency of loop()
    // therefore, it may jump back and forth between two speeds
    stepper1.runSpeed();
+   stepper2.runSpeed();
 }
 
 void update_motor_speed(int new_speed)
@@ -57,6 +82,20 @@ void update_motor_speed(int new_speed)
    if (curr_speed != new_speed)
    {
       stepper1.setSpeed(new_speed);
+      stepper2.setSpeed(new_speed / 12); // the hour hand speed is 1/12 of the minute
       curr_speed = new_speed;
+   }
+}
+
+void update_time_difference(bool is_update)
+{
+   if (is_update) // advance the time
+   {
+      stepper2.moveTo(stepper2.currentPosition() + total_steps / 12 * time_difference);
+   }
+   else
+   { // show current time again
+
+      stepper2.moveTo(stepper2.currentPosition() - total_steps / 12 * time_difference);
    }
 }
--- zhuoyue-motor/zhuoyue-python.py
+++ zhuoyue-motor/zhuoyue-python.py
@@ -4,30 +4,45 @@
 import cv2
 import serial, time
 
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-print("ssss")
-print(cv2.data.haarcascades)
+# side face detection (indeed...only works with side face, but it actually only works with one side)
+# but we can flip the image to detect the other direction.
+# image = cv2.flip(image, 1)
+is_detecting_side_face = True
+if is_detecting_side_face:
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_profileface.xml')
+else:
+    # front face detection
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
 # pathZ= '/Library/Frameworks/Python.framework/Versions/3.9/lib/python3.9/site-packages/cv2/data/haarcascade_frontalface_default.xml'
 # Change cv2.VideoCapture(1) to cv2.VideoCapture(0). It is a MacOS bug for Coaca applications
 # https://github.com/Classical-machine-learning/invisiblityCloak/issues/3
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
 # ArduinoSerial = serial.Serial('/dev/cu.usbserial-120', 9600, timeout=0.1)
 curr_num_faces = 0
 
-while cap.isOpened():
-    ret, frame = cap.read()
-    frame = cv2.flip(frame, 1)  # mirror the image
+
+def count_num_faces(frame):
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.1, 6)  # detect the face
-    num_faces = len(faces)
+    for x, y, w, h in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 3)
+    cv2.imshow('img', frame)
+    return len(faces)
+
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    num_faces = count_num_faces(frame)
+    if is_detecting_side_face:
+        frame = cv2.flip(frame, 1)  # mirror the image
+        num_faces += count_num_faces(frame)
     string = '{0:d}'.format(num_faces)
     print(string)
     if num_faces != curr_num_faces:
         # ArduinoSerial.write(string.encode('utf-8'))
         curr_num_faces = num_faces
-    for x, y, w, h in faces:
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 3)
-    cv2.imshow('img', frame)
+
     # press q to Quit
     if cv2.waitKey(10) & 0xFF == ord('q'):
         break
