27d8b11f1d2b0f1939ac878cf4761ce1f1cb7568
Zhouyue_Lyu <zhuoyue.lyu@gmail.com>
2022-12-15 07:23:42 -0500

    python streaming runnable code

--- zhuoyue-motor/zhuoyue-python.py
+++ zhuoyue-motor/zhuoyue-python.py
@@ -1,65 +1,88 @@
-# Face tracker using OpenCV and Arduino
-# by Shubham Santosh
-
 import cv2
-import serial, time
-from scipy import ndimage
+import numpy as np
 
-# side face detection (indeed...only works with side face, but it actually only works with one side)
-# but we can flip the image to detect the other direction.
-# image = cv2.flip(image, 1)
-is_detecting_side_face = True
-if is_detecting_side_face:
-    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_profileface.xml')
-else:
-    # front face detection
-    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+import requests
 
-# pathZ= '/Library/Frameworks/Python.framework/Versions/3.9/lib/python3.9/site-packages/cv2/data/haarcascade_frontalface_default.xml'
-# Change cv2.VideoCapture(1) to cv2.VideoCapture(0). It is a MacOS bug for Coaca applications
-# https://github.com/Classical-machine-learning/invisiblityCloak/issues/3
-cap = cv2.VideoCapture(1)
-ArduinoSerial = serial.Serial('/dev/cu.usbserial-1110', 9600, timeout=0.1)
-curr_num_faces = 0
+'''
+INFO SECTION
+- if you want to monitor raw parameters of ESP32CAM, open the browser and go to http://192.168.x.x/status
+- command can be sent through an HTTP get composed in the following way http://192.168.x.x/control?var=VARIABLE_NAME&val=VALUE (check varname and value in status)
+'''
 
+# ESP32 URL
+URL = "http://192.168.4.1"
+AWB = True
 
-def count_num_faces(frame):
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.1, 6)  # detect the face
-    for x, y, w, h in faces:
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 3)
-    cv2.imshow('img', frame)
-    return len(faces)
+# Face recognition and opencv setup
+cap = cv2.VideoCapture(URL + ":81/stream")
+face_classifier = cv2.CascadeClassifier(cv2.data.haarcascades +
+    'haarcascade_frontalface_alt.xml')  # insert the full path to haarcascade file if you encounter any problem
 
 
-while cap.isOpened():
-    ret, frame = cap.read()
+def set_resolution(url: str, index: int = 1, verbose: bool = False):
+    try:
+        if verbose:
+            resolutions = "10: UXGA(1600x1200)\n9: SXGA(1280x1024)\n8: XGA(1024x768)\n7: SVGA(800x600)\n6: VGA(640x480)\n5: CIF(400x296)\n4: QVGA(320x240)\n3: HQVGA(240x176)\n0: QQVGA(160x120)"
+            print("available resolutions\n{}".format(resolutions))
 
-    h, w, _ = frame.shape
-    # rotate image
-    frame = ndimage.rotate(frame, -45)
+        if index in [10, 9, 8, 7, 6, 5, 4, 3, 0]:
+            requests.get(url + "/control?var=framesize&val={}".format(index))
+        else:
+            print("Wrong index")
+    except:
+        print("SET_RESOLUTION: something went wrong")
 
-    # cropped the center part in the image (because it's in the wrong shaped after rotated 45 degrees
-    # center = frame.shape
-    # w = 750
-    # h = 750
-    # x = center[1] / 2 - w / 2
-    # y = center[0] / 2 - h / 2
-    # frame = frame[int(y):int(y + h), int(x):int(x + w)]
-    frame = frame[0:1200, 0:1200]
 
-    num_faces = count_num_faces(frame)
-    if is_detecting_side_face:
-        frame = cv2.flip(frame, 1)  # mirror the image
-        num_faces += count_num_faces(frame)
-    string = '{0:d}'.format(num_faces)
-    print(string)
-    if num_faces != curr_num_faces:
-        ArduinoSerial.write(string.encode('utf-8'))
-        curr_num_faces = num_faces
+def set_quality(url: str, value: int = 1, verbose: bool = False):
+    try:
+        if value >= 10 and value <= 63:
+            requests.get(url + "/control?var=quality&val={}".format(value))
+    except:
+        print("SET_QUALITY: something went wrong")
 
-    # press q to Quit
-    if cv2.waitKey(10) & 0xFF == ord('q'):
-        break
-cap.release()
-cv2.destroyAllWindows()
+
+def set_awb(url: str, awb: int = 1):
+    try:
+        awb = not awb
+        requests.get(url + "/control?var=awb&val={}".format(1 if awb else 0))
+    except:
+        print("SET_QUALITY: something went wrong")
+    return awb
+
+
+if __name__ == '__main__':
+    set_resolution(URL, index=8)
+
+    while True:
+        if cap.isOpened():
+            ret, frame = cap.read()
+
+            if ret:
+                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                gray = cv2.equalizeHist(gray)
+
+                faces = face_classifier.detectMultiScale(gray)
+                for (x, y, w, h) in faces:
+                    center = (x + w // 2, y + h // 2)
+                    frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 255, 0), 4)
+
+            cv2.imshow("frame", frame)
+
+            key = cv2.waitKey(1)
+
+            if key == ord('r'):
+                idx = int(input("Select resolution index: "))
+                set_resolution(URL, index=idx, verbose=True)
+
+            elif key == ord('q'):
+                val = int(input("Set quality (10 - 63): "))
+                set_quality(URL, value=val)
+
+            elif key == ord('a'):
+                AWB = set_awb(URL, AWB)
+
+            elif key == 27:
+                break
+
+    cv2.destroyAllWindows()
+    cap.release()
