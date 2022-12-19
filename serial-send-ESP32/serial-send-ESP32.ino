5120c42e4de6d1e6d024501470296c7bb038d5eb
Zhouyue_Lyu <zhuoyue.lyu@gmail.com>
2022-12-19 02:23:17 -0500

    serial connections worked!

---
+++ serial-send-ESP32/serial-send-ESP32.ino
@@ -0,0 +1,14 @@
+char mystr[5] = "Hiii"; //String data
+
+void setup() {
+  // Begin the Serial at 9600 Baud
+  Serial.begin(9600);
+}
+
+void loop() {
+  Serial.write(mystr,5); //Write the serial data
+  // digitalWrite(4, HIGH);
+  delay(1000);
+  // digitalWrite(4, LOW);
+  delay(1000);
+}
